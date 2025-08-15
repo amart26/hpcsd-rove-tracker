7fe6bb180f29552e03139f281034da922aefe83a
Anthony <58822386+amart26@users.noreply.github.com>
2025-08-15 17:25:22 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const nameInput = document.getElementById('first-floor-maint-t')
+
+// Get all buttons by their IDs
+const btn1 = document.getElementById('btn-1f-maint');
+const btn2 = document.getElementById('btn-1f-elv');
+const btn3 = document.getElementById('btn-300-pod');
+const btn4 = document.getElementById('btn-500-pod');
+const btn5 = document.getElementById('btn-2f-maint');
+const btn6 = document.getElementById('btn-2f-elv');
+const btn7 = document.getElementById('btn-400-pod');
+const btn8 = document.getElementById('btn-600-pod');
+
+// Log them to confirm
+console.log(btn1);
+
+// Example: Add a click event to one of them
